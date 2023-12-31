9bcc500edf0ab11e0c4cd1efc46897e5eb4c3786
Jakob <mail@bauer-jakob.de>
2023-12-31 13:30:38 +0100

    Another try

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,22 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bauerjakob.github.io',
-    base: '/weintreff'
-});
+    base: '/weintreff',
+    integrations: [sitemap(), image({
+        serviceEntryPoint: "@astrojs/image/sharp"
+    }), compress({
+        css: false,
+        html: {
+            removeComments: true,
+            removeAttributeQuotes: false
+        },
+        img: false,
+        js: false,
+        svg: true
+    }), prefetch()],
+    vite: {
+        build: {
+            assetsInlineLimit: 0
+        }
+    },
+},);
