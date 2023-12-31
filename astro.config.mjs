0ee48fe1b5da7245a2e83dceeaecc5807b7f5796
Jakob <mail@bauer-jakob.de>
2023-12-31 14:34:22 +0100

    Docker is now workin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,10 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bauerjakob.github.io',
-    base: '/weintreff',
-    integrations: [sitemap(), image({
-        serviceEntryPoint: "@astrojs/image/sharp"
-    }), compress({
-        css: false,
-        html: {
-            removeComments: true,
-            removeAttributeQuotes: false
-        },
-        img: false,
-        js: false,
-        svg: true
-    }), prefetch()],
-    vite: {
-        build: {
-            assetsInlineLimit: 0
-        }
-    },
-},);
+    output: "server",
+    adapter: node({
+        mode: "standalone"
+    })
+});
